49bbe12c58f3b04412eb9777e8ed8285fb6d35d0
MayconD27 <mdsx627@gmail.com>
2024-10-12 16:34:07 -0300

    criado corpo padrão da tela inicial

--- cadastro/etapas/passo01.php
+++ cadastro/etapas/passo01.php
@@ -1,2 +1,10 @@
-<h1>Dados do cliente </h1>
-<input type="text" id="nome">
+<h1>Dados do cliente <i class="bi bi-person"></i> </h1>
+<input type="text" id="nome" name="nome-cli" placeholder="Nome do cliente">
+
+<div class="tel">
+    <input type="text" placeholder="DDD" class="ddd" name="ddd" maxlength=2> 
+    <input type="text" placeholder="Telefone (Apenas números)" name="nm-tel" class="num-tel">
+</div>
+<input type="text" placeholder="procedimento">
+
+<p id="process">**Por favor, descreva o procedimento a ser realizado, como por exemplo escova progressiva, corte, coloração, etc.</p>
